48c4f8d2f5a0da1863c7ffc9eafea2e628ff182f
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-07 00:28:01 +0300

    Create the rotation animation component

---
+++ Source/TrickyAnimationComponents/Private/RotationAnimationComponent.cpp
@@ -0,0 +1,25 @@
+﻿// MIT License. Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "RotationAnimationComponent.h"
+
+URotationAnimationComponent::URotationAnimationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+
+void URotationAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	bRotateX ? DeltaRotator.Roll = DeltaRotationX : DeltaRotator.Roll = 0.f;
+	bRotateY ? DeltaRotator.Pitch = DeltaRotationY : DeltaRotator.Pitch = 0.f;
+	bRotateZ ? DeltaRotator.Yaw = DeltaRotationZ : DeltaRotator.Yaw = 0.f;
+
+	if (DeltaRotator.Equals(FRotator::ZeroRotator))
+	{
+		AddLocalRotation(DeltaRotator);
+	}
+}
---
+++ Source/TrickyAnimationComponents/Public/RotationAnimationComponent.h
@@ -0,0 +1,42 @@
+﻿// MIT License. Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "RotationAnimationComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRICKYANIMATIONCOMPONENTS_API URotationAnimationComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:
+	URotationAnimationComponent();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation)
+	bool bRotateX = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation, meta=(EditCondition="bRotateX"))
+	float DeltaRotationX = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation)
+	bool bRotateY = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation, meta=(EditCondition="bRotateY"))
+	float DeltaRotationY = 1.f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation)
+	bool bRotateZ = true;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation, meta=(EditCondition="bRotateZ"))
+	float DeltaRotationZ = 1.f;
+
+protected:
+	UPROPERTY()
+	FRotator DeltaRotator{FRotator::ZeroRotator};
+};
