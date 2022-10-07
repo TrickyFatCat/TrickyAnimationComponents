e15f19748d4f0b1d8e9d276efb4bb0f753337af8
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-07 20:34:40 +0300

    Implement base functionality

--- Source/TrickyAnimationComponents/Private/FloatingAnimationComponent.cpp
+++ Source/TrickyAnimationComponents/Private/FloatingAnimationComponent.cpp
@@ -12,11 +12,44 @@ UFloatingAnimationComponent::UFloatingAnimationComponent()
 void UFloatingAnimationComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	InitialLocation = GetRelativeLocation();
 }
 
 
-void UFloatingAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+void UFloatingAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);;
+
+	if (GetWorld())
+	{
+		const float Time = GetWorld()->GetTimeSeconds();
+		FVector CurrentLocation = GetRelativeLocation();
+
+		auto Animate = [&](const bool bIsAnimated,
+		                   float& Value,
+		                   const float& InitialValue,
+		                   const float& Amplitude,
+		                   const float& Frequency)
+		{
+			if (bIsAnimated)
+			{
+				Value = InitialValue + Amplitude * FMath::Sin(Frequency * Time);
+			}
+		};
+		
+		Animate(bAnimateX, CurrentLocation.X, InitialLocation.X, Amplitude.X, Frequency.X);
+		Animate(bAnimateY, CurrentLocation.Y, InitialLocation.Y, Amplitude.Y, Frequency.Y);
+		Animate(bAnimateZ, CurrentLocation.Z, InitialLocation.Z, Amplitude.Z, Frequency.Z);
+		
+		SetRelativeLocation(CurrentLocation);
+	}
 }
 
+void UFloatingAnimationComponent::SetIsAnimated(const bool bX, const bool bY, const bool bZ)
+{
+	bAnimateX = bX;
+	bAnimateY = bY;
+	bAnimateZ = bZ;
+}
--- Source/TrickyAnimationComponents/Public/FloatingAnimationComponent.h
+++ Source/TrickyAnimationComponents/Public/FloatingAnimationComponent.h
@@ -7,17 +7,40 @@
 #include "FloatingAnimationComponent.generated.h"
 
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class TRICKYANIMATIONCOMPONENTS_API UFloatingAnimationComponent : public USceneComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	UFloatingAnimationComponent();
 
 protected:
 	virtual void BeginPlay() override;
 
-public:	
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyAnimations|FloatingAnimaton")
+	void SetIsAnimated(const bool bX, const bool bY, const bool bZ);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+	FVector Frequency{FVector::UpVector};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation")
+	FVector Amplitude{FVector::UpVector};
+	
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Animation")
+	bool bAnimateX = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Animation")
+	bool bAnimateY = false;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Animation")
+	bool bAnimateZ = true;
+private:
+	FVector InitialLocation{FVector::ZeroVector};
 };
