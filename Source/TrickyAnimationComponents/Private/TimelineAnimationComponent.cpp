a039833496799f0b786fd5099c0da02075d53a1e
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-09 00:10:43 +0300

    Create basic functions

--- Source/TrickyAnimationComponents/Private/TimelineAnimationComponent.cpp
+++ Source/TrickyAnimationComponents/Private/TimelineAnimationComponent.cpp
@@ -3,22 +3,67 @@
 
 #include "TimelineAnimationComponent.h"
 
+#include "Components/TimelineComponent.h"
+
 
 UTimelineAnimationComponent::UTimelineAnimationComponent()
 {
 	PrimaryComponentTick.bCanEverTick = false;
+
+	Timeline = CreateDefaultSubobject<UTimelineComponent>("Timeline");
 }
 
 
 void UTimelineAnimationComponent::BeginPlay()
 {
 	Super::BeginPlay();
+
+	CurrentState = InitialState;
 }
 
 
-void UTimelineAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType,
-                                                FActorComponentTickFunction* ThisTickFunction)
+void UTimelineAnimationComponent::Start()
 {
-	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (CurrentState != ETimelineAnimationState::Transition)
+	{
+		TargetState = CurrentState == ETimelineAnimationState::Start
+			              ? ETimelineAnimationState::End
+			              : ETimelineAnimationState::Start;
+
+		//Start timeline here;
+		CurrentState = ETimelineAnimationState::Transition;
+		OnAnimationStarted.Broadcast(TargetState);
+	}
 }
 
+void UTimelineAnimationComponent::Reverse()
+{
+	if (CurrentState == ETimelineAnimationState::Transition && bIsReversible)
+	{
+		TargetState = TargetState == ETimelineAnimationState::Start
+			              ? ETimelineAnimationState::End
+			              : ETimelineAnimationState::Start;
+		// Reverse timeline here;
+		OnAnimationReversed.Broadcast(TargetState);
+	}
+}
+
+void UTimelineAnimationComponent::Stop()
+{
+	// Do checks
+	// Do logic
+	OnAnimationStopped.Broadcast();
+}
+
+void UTimelineAnimationComponent::Resume()
+{
+	// Do checks
+	// Do logic
+	OnAnimationResumed.Broadcast();
+}
+
+void UTimelineAnimationComponent::FinishAnimation()
+{
+	CurrentState = TargetState;
+	OnAnimationFinished.Broadcast(CurrentState);
+}
--- Source/TrickyAnimationComponents/Public/TimelineAnimationComponent.h
+++ Source/TrickyAnimationComponents/Public/TimelineAnimationComponent.h
@@ -6,6 +6,27 @@
 #include "Components/ActorComponent.h"
 #include "TimelineAnimationComponent.generated.h"
 
+class UCurveFloat;
+class UTimelineComponent;
+
+UENUM(BlueprintType)
+enum class ETimelineAnimationState: uint8
+{
+	Start,
+	End,
+	Transition UMETA(Hidden),
+	Stopped	UMETA(Hdidden)
+};
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAnimationStartedSignature, ETimelineAnimationState, TargetState);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAnimationReversedSignature, ETimelineAnimationState, NewTargetState);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnAnimationFinishedSignature, ETimelineAnimationState, NewState);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAnimationStoppedSignature);
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnAnimationResumedSignature);
 
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class TRICKYANIMATIONCOMPONENTS_API UTimelineAnimationComponent : public UActorComponent
@@ -18,7 +39,53 @@ public:
 protected:
 	virtual void BeginPlay() override;
 
+private:
+	UPROPERTY()
+	UTimelineComponent* Timeline = nullptr;
+
 public:
-	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
-	                           FActorComponentTickFunction* ThisTickFunction) override;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Animation)
+	ETimelineAnimationState InitialState = ETimelineAnimationState::Start;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyAnimations|TimelineAnimation")
+	FOnAnimationStartedSignature OnAnimationStarted;
+	
+	UPROPERTY(BlueprintAssignable, Category="TrickyAnimations|TimelineAnimation")
+	FOnAnimationReversedSignature OnAnimationReversed;
+	
+	UPROPERTY(BlueprintAssignable, Category="TrickyAnimations|TimelineAnimation")
+	FOnAnimationFinishedSignature OnAnimationFinished;
+
+	UPROPERTY(BlueprintAssignable, Category="TrickyAnimations|TimelineAnimation")
+	FOnAnimationStoppedSignature OnAnimationStopped;
+	
+	UPROPERTY(BlueprintAssignable, Category="TrickyAnimations|TimelineAnimation")
+	FOnAnimationResumedSignature OnAnimationResumed;
+
+	UFUNCTION(BlueprintCallable, Category="TrickyAnimations|TimelineAnimation")
+	void Start();
+
+	UFUNCTION(BlueprintCallable, Category="TrickyAnimations|TimelineAnimation")
+	void Reverse();
+
+	UFUNCTION(BlueprintCallable, Category="TrickyAnimations|TimelineAnimation")
+	void Stop();
+
+	UFUNCTION(BlueprintCallable, Category="TrickyAnimations|TimelineAnimation")
+	void Resume();
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category="Animation")
+	bool bIsReversible = false;
+
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Animation|States", meta=(AllowPrivateAccess="true"))
+	ETimelineAnimationState CurrentState = InitialState;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="Animation|States", meta=(AllowPrivateAccess="true"))
+	ETimelineAnimationState TargetState = ETimelineAnimationState::End;
+
+	UFUNCTION()
+	void FinishAnimation();
+
 };
