72008f9f3c5bea0181051b587817fcdcc9d81c2b
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-08 23:25:32 +0300

    Create a timeline animation component class

---
+++ Source/TrickyAnimationComponents/Private/TimelineAnimationComponent.cpp
@@ -0,0 +1,24 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+
+#include "TimelineAnimationComponent.h"
+
+
+UTimelineAnimationComponent::UTimelineAnimationComponent()
+{
+	PrimaryComponentTick.bCanEverTick = false;
+}
+
+
+void UTimelineAnimationComponent::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+
+void UTimelineAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType,
+                                                FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+}
+
---
+++ Source/TrickyAnimationComponents/Public/TimelineAnimationComponent.h
@@ -0,0 +1,24 @@
+﻿// MIT License Copyright (c) 2022 Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "TimelineAnimationComponent.generated.h"
+
+
+UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
+class TRICKYANIMATIONCOMPONENTS_API UTimelineAnimationComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:
+	UTimelineAnimationComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+	                           FActorComponentTickFunction* ThisTickFunction) override;
+};
