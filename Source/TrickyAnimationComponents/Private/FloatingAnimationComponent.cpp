2be034191c06755a8e52a55e67a0d3c58d50148d
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-16 14:21:34 +0300

    Fix variable name

--- Source/TrickyAnimationComponents/Private/FloatingAnimationComponent.cpp
+++ Source/TrickyAnimationComponents/Private/FloatingAnimationComponent.cpp
@@ -29,7 +29,7 @@ void UFloatingAnimationComponent::TickComponent(float DeltaTime,
 	{
 		FVector CurrentLocation = GetRelativeLocation();
 
-		Float(bAnimateX, CurrentLocation.X, InitialLocation.X, Amplitude.X, Frequency.X);
+		Float(bFloatX, CurrentLocation.X, InitialLocation.X, Amplitude.X, Frequency.X);
 		Float(bFloatY, CurrentLocation.Y, InitialLocation.Y, Amplitude.Y, Frequency.Y);
 		Float(bFloatZ, CurrentLocation.Z, InitialLocation.Z, Amplitude.Z, Frequency.Z);
 
@@ -39,7 +39,7 @@ void UFloatingAnimationComponent::TickComponent(float DeltaTime,
 
 void UFloatingAnimationComponent::SetIsFloating(const bool bX, const bool bY, const bool bZ)
 {
-	bAnimateX = bX;
+	bFloatX = bX;
 	bFloatY = bY;
 	bFloatZ = bZ;
 
@@ -61,5 +61,5 @@ void UFloatingAnimationComponent::Float(const bool bAxisAnimated,
 
 void UFloatingAnimationComponent::ToggleTick()
 {
-	SetComponentTickEnabled((!bAnimateX && !bFloatY && !bFloatZ) || (bAnimateX || bFloatY || bFloatZ));
+	SetComponentTickEnabled((!bFloatX && !bFloatY && !bFloatZ) || (bFloatX || bFloatY || bFloatZ));
 }
