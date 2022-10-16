841b8eaa6ed5cfe5cd03d44f09a5d3fb672a47b7
Artyom Volkov <tricky.fat.cat@gmail.com>
2022-10-16 14:24:09 +0300

    Add comments

--- Source/TrickyAnimationComponents/Public/RotationAnimationComponent.h
+++ Source/TrickyAnimationComponents/Public/RotationAnimationComponent.h
@@ -36,15 +36,27 @@ public:
 	void SetIsRotating(const bool bX, const bool bY, const bool bZ);
 
 protected:
+	/**
+	 * Toggles rotation along the X axis.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation)
 	bool bRotateX = false;
 
+	/**
+	 * Toggles rotation along the Y axis.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation)
 	bool bRotateY = false;
 
+	/**
+	 * Toggles rotation along the Z axis.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category=Rotation)
 	bool bRotateZ = true;
 
+	/**
+	 * Determines the rotation speed along each axis.
+	 */
 	UPROPERTY(EditAnywhere, BlueprintGetter=GetRotationSpeed, BlueprintSetter=SetRotationSpeed, Category=Rotation)
 	FRotator RotationSpeed{0.f, 1.f, 0.f};
 
