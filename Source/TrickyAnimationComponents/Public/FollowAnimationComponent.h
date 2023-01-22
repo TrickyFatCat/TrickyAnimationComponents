0142a92278e7cd40402893bd123d578faf6e2e8a
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-01-22 23:01:29 +0200

    Update comments

--- Source/TrickyAnimationComponents/Public/FollowAnimationComponent.h
+++ Source/TrickyAnimationComponents/Public/FollowAnimationComponent.h
@@ -8,7 +8,7 @@
 
 /**
  * A component which interpolates an owning actor location towards a target location/actor.
- */
+*/
 UCLASS(ClassGroup=(Custom), meta=(BlueprintSpawnableComponent))
 class TRICKYANIMATIONCOMPONENTS_API UFollowAnimationComponent : public UActorComponent
 {
@@ -17,48 +17,40 @@ class TRICKYANIMATIONCOMPONENTS_API UFollowAnimationComponent : public UActorCom
 public:
 	UFollowAnimationComponent();
 
-	/**
-	 * Toggles if the component will interpolate position towards a target actor or a specific location.
-	 */
+	/**Toggles if the component will interpolate position towards a target actor or a specific location.*/
 	UPROPERTY(EditAnywhere,
 		BlueprintReadWrite,
 		Category="Animation",
 		meta=(DisplayAfter="bIsEnabled"))
 	bool bFollowActor = true;
 
-	/**
-	 * An actor which world location will be used as a target location.
-	 */
+	/**An actor which world location will be used as a target location.*/
 	UPROPERTY(EditAnywhere,
 		BlueprintReadWrite,
 		Category="Animation",
 		meta=(DisplayAfter="bIsEnabled", EditCondition="bFollowActor", EditConditionHides))
 	AActor* TargetActor = nullptr;
 
-	/**
-	 * The world location which will be used as a target location.
-	 */
+	/**The world location which will be used as a target location.*/
 	UPROPERTY(EditAnywhere,
 		BlueprintReadWrite,
 		Category="Animation",
 		meta=(DisplayAfter="bIsEnabled", EditCondition="!bFollowActor", EditConditionHides))
 	FVector Location{FVector::ZeroVector};
 
-	/**
-	 * Location offset relative to the target location.
-	 */
+	/**Location offset relative to the target location.*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation", meta=(DisplayAfter="bIsEnabled"))
 	FVector TargetLocationOffset{FVector::ZeroVector};
 
-	/**
-	 * Determines the speed of interpolation.
-	 */
+	/**Determines the speed of interpolation.*/
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Animation", meta=(DisplayAfter="bIsEnabled"))
 	float Speed = 5;
 
+	/**Starts interpolation.*/
 	UFUNCTION(BlueprintSetter, Category="TrickyAnimations|EaseAnimation")
 	bool StartFollowing();
 
+	/**Stops interpolation.*/
 	UFUNCTION(BlueprintSetter, Category="TrickyAnimations|EaseAnimation")
 	bool StopFollowing();
 
