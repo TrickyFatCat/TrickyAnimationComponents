adb78434c1645558875c73e65b94b91cc88a0ad4
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-30 23:41:14 +0300

    Update calculations

--- Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
+++ Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
@@ -5,15 +5,15 @@
 
 double UTrickyEasingLibrary::EaseInSine(const double Alpha)
 {
-	return 1.f - FMath::Cos((Alpha * PI) / 2.f);
+	return 1.f - FMath::Cos((Alpha * PI) * 0.5f);
 }
 
 double UTrickyEasingLibrary::EaseOutSine(const double Alpha)
 {
-	return FMath::Sin((Alpha * PI) / 2.f);
+	return FMath::Sin((Alpha * PI) * 0.5f);
 }
 
 double UTrickyEasingLibrary::EaseInOutSine(const double Alpha)
 {
-	return -(FMath::Cos(PI * Alpha) - 1.f) / 2.f;
+	return (1 - FMath::Cos(Alpha * PI)) * 0.5f;
 }
