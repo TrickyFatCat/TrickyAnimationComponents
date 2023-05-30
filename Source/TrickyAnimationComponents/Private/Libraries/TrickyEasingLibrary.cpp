ceb88f276ca5e3df9f461833c549648af1f75078
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-30 23:14:51 +0300

    Create TrickyEasingLibrary

---
+++ Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
@@ -0,0 +1,19 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+
+#include "Libraries/TrickyEasingLibrary.h"
+
+double UTrickyEasingLibrary::EaseInSine(const double Alpha)
+{
+	return 1.f - FMath::Cos((Alpha * PI) / 2.f);
+}
+
+double UTrickyEasingLibrary::EaseOutSine(const double Alpha)
+{
+	return FMath::Sin((Alpha * PI) / 2.f);
+}
+
+double UTrickyEasingLibrary::EaseInOutSine(const double Alpha)
+{
+	return -(FMath::Cos(PI * Alpha) - 1.f) / 2.f;
+}
---
+++ Source/TrickyAnimationComponents/Public/Libraries/TrickyEasingLibrary.h
@@ -0,0 +1,26 @@
+﻿// MIT License Copyright. Created by Artyom "Tricky Fat Cat" Volkov
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "TrickyEasingLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TRICKYANIMATIONCOMPONENTS_API UTrickyEasingLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+public:
+	UFUNCTION(BlueprintPure, Category="TrickyEasing")
+	static double EaseInSine(const double Alpha = 0.f);
+	
+	UFUNCTION(BlueprintPure, Category="TrickyEasing")
+	static double EaseOutSine(const double Alpha = 0.f);
+	
+	UFUNCTION(BlueprintPure, Category="TrickyEasing")
+	static double EaseInOutSine(const double Alpha = 0.f);
+};
