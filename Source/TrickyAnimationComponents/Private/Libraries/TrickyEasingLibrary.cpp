9fd58b434ad179fc12ab036e12afe7d2d3928055
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-05-31 21:59:12 +0300

    Add new easing function

--- Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
+++ Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
@@ -15,5 +15,20 @@ double UTrickyEasingLibrary::EaseOutSine(const double Alpha)
 
 double UTrickyEasingLibrary::EaseInOutSine(const double Alpha)
 {
-	return (1 - FMath::Cos(Alpha * PI)) * 0.5f;
+	return (1.f - FMath::Cos(Alpha * PI)) * 0.5f;
+}
+
+double UTrickyEasingLibrary::EaseInCubic(const double Alpha)
+{
+	return Alpha * Alpha * Alpha;
+}
+
+double UTrickyEasingLibrary::EaseOutCubic(const double Alpha)
+{
+	return 1.f - FMath::Pow(1.f - Alpha, 3);
+}
+
+double UTrickyEasingLibrary::EaseInOutCubic(const double Alpha)
+{
+	return Alpha < 0.5 ? 4.f * Alpha * Alpha * Alpha : 1.f - FMath::Pow(-2.f * Alpha + 2.f, 3) * 0.5f;
 }
--- Source/TrickyAnimationComponents/Public/Libraries/TrickyEasingLibrary.h
+++ Source/TrickyAnimationComponents/Public/Libraries/TrickyEasingLibrary.h
@@ -15,12 +15,21 @@ class TRICKYANIMATIONCOMPONENTS_API UTrickyEasingLibrary : public UBlueprintFunc
 	GENERATED_BODY()
 
 public:
-	UFUNCTION(BlueprintPure, Category="TrickyEasing")
+	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInSine")
 	static double EaseInSine(const double Alpha = 0.f);
 	
-	UFUNCTION(BlueprintPure, Category="TrickyEasing")
+	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutSine")
 	static double EaseOutSine(const double Alpha = 0.f);
 	
-	UFUNCTION(BlueprintPure, Category="TrickyEasing")
+	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutSine")
 	static double EaseInOutSine(const double Alpha = 0.f);
+	
+	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInCubic")
+	static double EaseInCubic(const double Alpha = 0.f);
+	
+	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutCubic")
+	static double EaseOutCubic(const double Alpha = 0.f);
+	
+	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutCubic")
+	static double EaseInOutCubic(const double Alpha = 0.f);
 };
