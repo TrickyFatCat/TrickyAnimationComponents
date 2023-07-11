8780cdc5c1e6ef4b0f796c0847d7107c57b868e2
Artyom Volkov <tricky.fat.cat@gmail.com>
2023-07-11 22:46:40 +0300

    Rename enum and add new functions

--- Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
+++ Source/TrickyAnimationComponents/Private/Libraries/TrickyEasingLibrary.cpp
@@ -3,168 +3,258 @@
 
 #include "Libraries/TrickyEasingLibrary.h"
 
-double UTrickyEasingLibrary::Ease(const EEaseType EaseType, const double Alpha)
+double UTrickyEasingLibrary::Ease(const EEaseFunction EaseFunction, const double Alpha)
 {
 	double Result = 0.f;
-	switch (EaseType)
+	switch (EaseFunction)
 	{
-	case EEaseType::Linear:
+	case EEaseFunction::Linear:
 		Result = Alpha;
 		break;
 
-	case EEaseType::InSine:
+	case EEaseFunction::InSine:
 		Result = EaseInSine(Alpha);
 		break;
 
-	case EEaseType::OutSine:
+	case EEaseFunction::OutSine:
 		Result = EaseOutSine(Alpha);
 		break;
 
-	case EEaseType::InOutSine:
+	case EEaseFunction::InOutSine:
 		Result = EaseInOutSine(Alpha);
 		break;
 
-	case EEaseType::InQuad:
+	case EEaseFunction::InQuad:
 		Result = EaseInQuad(Alpha);
 		break;
 
-	case EEaseType::OutQuad:
+	case EEaseFunction::OutQuad:
 		Result = EaseOutQuad(Alpha);
 		break;
 
-	case EEaseType::InOutQuad:
+	case EEaseFunction::InOutQuad:
 		Result = EaseInOutQuad(Alpha);
 		break;
 
-	case EEaseType::InCubic:
+	case EEaseFunction::InCubic:
 		Result = EaseInCubic(Alpha);
 		break;
 
-	case EEaseType::OutCubic:
+	case EEaseFunction::OutCubic:
 		Result = EaseOutCubic(Alpha);
 		break;
 
-	case EEaseType::InOutCubic:
+	case EEaseFunction::InOutCubic:
 		Result = EaseInOutCubic(Alpha);
 		break;
 
-	case EEaseType::InQuart:
+	case EEaseFunction::InQuart:
 		Result = EaseInQuart(Alpha);
 		break;
 
-	case EEaseType::OutQuart:
+	case EEaseFunction::OutQuart:
 		Result = EaseOutQuart(Alpha);
 		break;
 
-	case EEaseType::InOutQuart:
+	case EEaseFunction::InOutQuart:
 		Result = EaseInOutQuart(Alpha);
 		break;
 
-	case EEaseType::InQuint:
+	case EEaseFunction::InQuint:
 		Result = EaseInQuint(Alpha);
 		break;
 
-	case EEaseType::OutQuint:
+	case EEaseFunction::OutQuint:
 		Result = EaseOutQuint(Alpha);
 		break;
 
-	case EEaseType::InOutQuint:
+	case EEaseFunction::InOutQuint:
 		Result = EaseInOutQuint(Alpha);
 		break;
 
-	case EEaseType::InCirc:
+	case EEaseFunction::InCirc:
 		Result = EaseInCirc(Alpha);
 		break;
 
-	case EEaseType::OutCirc:
+	case EEaseFunction::OutCirc:
 		Result = EaseOutCirc(Alpha);
 		break;
 
-	case EEaseType::InOutCirc:
+	case EEaseFunction::InOutCirc:
 		Result = EaseInOutCirc(Alpha);
 		break;
 
-	case EEaseType::InExpo:
+	case EEaseFunction::InExpo:
 		Result = EaseInExpo(Alpha);
 		break;
 
-	case EEaseType::OutExpo:
+	case EEaseFunction::OutExpo:
 		Result = EaseOutExpo(Alpha);
 		break;
 
-	case EEaseType::InOutExpo:
+	case EEaseFunction::InOutExpo:
 		Result = EaseInOutExpo(Alpha);
 		break;
 
-	case EEaseType::InBack:
+	case EEaseFunction::InBack:
 		Result = EaseInBack(Alpha);
 		break;
 
-	case EEaseType::OutBack:
+	case EEaseFunction::OutBack:
 		Result = EaseOutBack(Alpha);
 		break;
 
-	case EEaseType::InOutBack:
+	case EEaseFunction::InOutBack:
 		Result = EaseInOutBack(Alpha);
 		break;
-		
-	case EEaseType::InElastic:
+
+	case EEaseFunction::InElastic:
 		Result = EaseInElastic(Alpha);
 		break;
 
-	case EEaseType::OutElastic:
+	case EEaseFunction::OutElastic:
 		Result = EaseOutElastic(Alpha);
 		break;
 
-	case EEaseType::InOutElastic:
+	case EEaseFunction::InOutElastic:
 		Result = EaseInOutElastic(Alpha);
 		break;
-		
-	case EEaseType::InBounce:
+
+	case EEaseFunction::InBounce:
 		Result = EaseInBounce(Alpha);
 		break;
 
-	case EEaseType::OutBounce:
+	case EEaseFunction::OutBounce:
 		Result = EaseOutBounce(Alpha);
 		break;
 
-	case EEaseType::InOutBounce:
+	case EEaseFunction::InOutBounce:
 		Result = EaseInOutBounce(Alpha);
 		break;
-		
+
 	default: ;
 	}
 
 	return Result;
 }
 
+double UTrickyEasingLibrary::EaseFloat(double A, double B, const EEaseFunction EaseFunction, const double Alpha)
+{
+	return FMath::Lerp(A, B, Ease(EaseFunction, Alpha));
+}
+
+FVector UTrickyEasingLibrary::EaseVector(FVector& A, FVector& B, const EEaseFunction EaseFunction, const double Alpha)
+{
+	FVector NewVector = FVector::Zero();
+
+	NewVector.X = FMath::Lerp(A.X, B.X, Ease(EaseFunction, Alpha));
+	NewVector.Y = FMath::Lerp(A.Y, B.Y, Ease(EaseFunction, Alpha));
+	NewVector.Z = FMath::Lerp(A.Z, B.Z, Ease(EaseFunction, Alpha));
+
+	return NewVector;
+}
+
+FRotator UTrickyEasingLibrary::EaseRotator(FRotator& A,
+                                           FRotator& B,
+                                           const EEaseFunction EaseFunction,
+                                           const double Alpha)
+{
+	FRotator NewRotator = FRotator(0.f);
+
+	NewRotator.Pitch = FMath::Lerp(A.Pitch, B.Pitch, Ease(EaseFunction, Alpha));
+	NewRotator.Yaw = FMath::Lerp(A.Yaw, B.Yaw, Ease(EaseFunction, Alpha));
+	NewRotator.Roll = FMath::Lerp(A.Roll, B.Roll, Ease(EaseFunction, Alpha));
+
+	return NewRotator;
+}
+
 double UTrickyEasingLibrary::EaseInSine(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f - FMath::Cos((Alpha * PI) * 0.5f);
 }
 
 double UTrickyEasingLibrary::EaseOutSine(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return FMath::Sin((Alpha * PI) * 0.5f);
 }
 
 double UTrickyEasingLibrary::EaseInOutSine(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return (1.f - FMath::Cos(Alpha * PI)) * 0.5f;
 }
 
 double UTrickyEasingLibrary::EaseInQuad(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha * Alpha;
 }
 
 double UTrickyEasingLibrary::EaseOutQuad(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f - (1.f - Alpha) * (1.f - Alpha);
 }
 
 double UTrickyEasingLibrary::EaseInOutQuad(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha < 0.5f
 		       ? 2.f * Alpha * Alpha
 		       : 1.f - FMath::Pow(2.f - 2.f * Alpha, 2) * 0.5f;
@@ -172,31 +262,91 @@ double UTrickyEasingLibrary::EaseInOutQuad(const double Alpha)
 
 double UTrickyEasingLibrary::EaseInCubic(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha * Alpha * Alpha;
 }
 
 double UTrickyEasingLibrary::EaseOutCubic(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f - FMath::Pow(1.f - Alpha, 3);
 }
 
 double UTrickyEasingLibrary::EaseInOutCubic(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha < 0.5 ? 4.f * Alpha * Alpha * Alpha : 1.f - FMath::Pow(-2.f * Alpha + 2.f, 3) * 0.5f;
 }
 
 double UTrickyEasingLibrary::EaseInQuart(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha * Alpha * Alpha * Alpha;
 }
 
 double UTrickyEasingLibrary::EaseOutQuart(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f - FMath::Pow(1.f - Alpha, 4);
 }
 
 double UTrickyEasingLibrary::EaseInOutQuart(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha < 0.5f
 		       ? 8.f * Alpha * Alpha * Alpha * Alpha
 		       : 1.f - FMath::Pow(2.f - 2.f * Alpha, 4) * 0.5f;
@@ -204,16 +354,46 @@ double UTrickyEasingLibrary::EaseInOutQuart(const double Alpha)
 
 double UTrickyEasingLibrary::EaseInQuint(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha * Alpha * Alpha * Alpha * Alpha;
 }
 
 double UTrickyEasingLibrary::EaseOutQuint(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f - FMath::Pow(1.f - Alpha, 5);
 }
 
 double UTrickyEasingLibrary::EaseInOutQuint(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha < 0.5f
 		       ? 16.f * Alpha * Alpha * Alpha * Alpha * Alpha
 		       : 1.f - FMath::Pow(2.f - 2.f * Alpha, 5.f) * 0.5f;
@@ -221,16 +401,46 @@ double UTrickyEasingLibrary::EaseInOutQuint(const double Alpha)
 
 double UTrickyEasingLibrary::EaseInCirc(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f - FMath::Sqrt(1.f - FMath::Pow(Alpha, 2));
 }
 
 double UTrickyEasingLibrary::EaseOutCirc(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return FMath::Sqrt(1.f - FMath::Pow(Alpha - 1.f, 2));
 }
 
 double UTrickyEasingLibrary::EaseInOutCirc(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha < 0.5f
 		       ? (1.f - FMath::Sqrt(1.f - FMath::Pow(2.f * Alpha, 2))) * 0.5f
 		       : (FMath::Sqrt(1.f - FMath::Pow(2.f - 2.f * Alpha, 2)) + 1) * 0.5f;
@@ -287,6 +497,16 @@ double UTrickyEasingLibrary::EaseInBack(const double Alpha)
 {
 	constexpr double Overshoot = PI;
 
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return Alpha * Alpha * ((Overshoot + 1.f) * Alpha - Overshoot);
 }
 
@@ -295,6 +515,16 @@ double UTrickyEasingLibrary::EaseOutBack(const double Alpha)
 	const double X = Alpha - 1;
 	constexpr double Overshoot = PI;
 
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f + X * X * (X * (Overshoot + 1.f) + Overshoot);
 }
 
@@ -302,6 +532,16 @@ double UTrickyEasingLibrary::EaseInOutBack(const double Alpha)
 {
 	const double Alpha2 = Alpha * 2.f;
 	constexpr double Overshoot = PI * 1.525;
+
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
 
 	if (Alpha < 0.5f)
 	{
@@ -316,16 +556,46 @@ double UTrickyEasingLibrary::EaseInElastic(const double Alpha)
 {
 	const double X = Alpha - 1.f;
 
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return -FMath::Pow(2.f, 10.f * X) * FMath::Sin((X * 40.f - 3.f) * PI / 6.f);
 }
 
 double UTrickyEasingLibrary::EaseOutElastic(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	return 1.f + FMath::Pow(2.f, 10.f * -Alpha) * FMath::Sin((-Alpha * 40.f - 3.f) * PI / 6.f);
 }
 
 double UTrickyEasingLibrary::EaseInOutElastic(const double Alpha)
 {
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
+
 	const double X = 2.f * Alpha - 1.f;
 
 	const double k = (80.f * X - 9.f) * PI / 18.f;
@@ -346,14 +616,13 @@ double UTrickyEasingLibrary::EaseInBounce(const double Alpha)
 	{
 		return 1.f;
 	}
-	
-	return  1.f - EaseInBounce(1.f - Alpha);
+
+	return 1.f - EaseInBounce(1.f - Alpha);
 }
 
 double UTrickyEasingLibrary::EaseOutBounce(const double Alpha)
 {
 	double Alpha2 = 0.f;
-
 	constexpr double Bounce_R = 1.0f / 2.75f;
 	constexpr double Bounce_K1 = Bounce_R;
 	constexpr double Bounce_K2 = 2 * Bounce_R;
@@ -362,6 +631,16 @@ double UTrickyEasingLibrary::EaseOutBounce(const double Alpha)
 	constexpr double Bounce_K5 = 2.25f * Bounce_R;
 	constexpr double Bounce_K6 = 2.625f * Bounce_R;
 	constexpr double Bounce_K0 = 7.5625f;
+
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
 
 	if (Alpha < Bounce_K1)
 	{
@@ -387,6 +666,16 @@ double UTrickyEasingLibrary::EaseOutBounce(const double Alpha)
 double UTrickyEasingLibrary::EaseInOutBounce(const double Alpha)
 {
 	const double Alpha2 = Alpha * 2.f;
+
+	if (Alpha <= 0.f)
+	{
+		return 0.f;
+	}
+
+	if (Alpha >= 1.f)
+	{
+		return 1.f;
+	}
 
 	return Alpha2 < 1.f
 		       ? 0.5f - 0.5f * EaseOutBounce(1.f - Alpha2)
--- Source/TrickyAnimationComponents/Public/Libraries/TrickyEasingLibrary.h
+++ Source/TrickyAnimationComponents/Public/Libraries/TrickyEasingLibrary.h
@@ -7,7 +7,7 @@
 #include "TrickyEasingLibrary.generated.h"
 
 UENUM(BlueprintType)
-enum class EEaseType : uint8
+enum class EEaseFunction : uint8
 {
 	Linear,
 	InSine,
@@ -41,6 +41,7 @@ enum class EEaseType : uint8
 	OutBounce,
 	InOutBounce
 };
+
 /**
  * 
  */
@@ -51,95 +52,110 @@ class TRICKYANIMATIONCOMPONENTS_API UTrickyEasingLibrary : public UBlueprintFunc
 
 public:
 	UFUNCTION(BlueprintPure, Category="TrickyEasing")
-	static double Ease(const EEaseType EaseType, const double Alpha = 0.f);
-	
+	static double Ease(const EEaseFunction EaseFunction, const double Alpha = 0.f);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEasing")
+	static double EaseFloat(double A, double B, const EEaseFunction EaseFunction, const double Alpha = 0.f);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEasing", meta=(AutoCreateRefTerm="A, B"))
+	static FVector EaseVector(UPARAM(ref) FVector& A,
+	                          UPARAM(ref) FVector& B,
+	                          const EEaseFunction EaseFunction,
+	                          const double Alpha = 0.f);
+
+	UFUNCTION(BlueprintCallable, Category="TrickyEasing", meta=(AutoCreateRefTerm="A, B"))
+	static FRotator EaseRotator(UPARAM(ref) FRotator& A,
+	                            UPARAM(ref) FRotator& B,
+	                            const EEaseFunction EaseFunction,
+	                            const double Alpha = 0.f);
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInSine")
 	static double EaseInSine(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutSine")
 	static double EaseOutSine(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutSine")
 	static double EaseInOutSine(const double Alpha = 0.f);
 
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInQuad")
 	static double EaseInQuad(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutQuad")
 	static double EaseOutQuad(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutQuad")
 	static double EaseInOutQuad(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInCubic")
 	static double EaseInCubic(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutCubic")
 	static double EaseOutCubic(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutCubic")
 	static double EaseInOutCubic(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInQuart")
 	static double EaseInQuart(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutQuart")
 	static double EaseOutQuart(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutQuart")
 	static double EaseInOutQuart(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInQuint")
 	static double EaseInQuint(const double Alpha = 0.f);
 
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutQuint")
 	static double EaseOutQuint(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutQuint")
 	static double EaseInOutQuint(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInCirc")
 	static double EaseInCirc(const double Alpha = 0.f);
 
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutCirc")
 	static double EaseOutCirc(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutCirc")
 	static double EaseInOutCirc(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInExpo")
 	static double EaseInExpo(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutExpo")
 	static double EaseOutExpo(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutExpo")
 	static double EaseInOutExpo(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInBack")
 	static double EaseInBack(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutBack")
 	static double EaseOutBack(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutBack")
 	static double EaseInOutBack(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInElastic")
 	static double EaseInElastic(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutElastic")
 	static double EaseOutElastic(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutElastic")
 	static double EaseInOutElastic(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInBounce")
 	static double EaseInBounce(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseOutBounce")
 	static double EaseOutBounce(const double Alpha = 0.f);
-	
+
 	UFUNCTION(BlueprintPure, Category="TrickyEasing", DisplayName="EaseInOutBounce")
 	static double EaseInOutBounce(const double Alpha = 0.f);
 };
